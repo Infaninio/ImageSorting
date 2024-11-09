b07f91eaa983ce444cbad8cbe6f4192a314f6aa3
Martin Engel <infaninio@gmail.com>
2024-11-09 14:51:36 +0100

    Format files.

--- WebUI/Image/__init__.py
+++ WebUI/Image/__init__.py
@@ -1,1 +1,2 @@
-from .image import Custom_Image
+"""Image package for the WebUi."""
+from .image import Custom_Image  # noqa: F401
--- test/test_image.py
+++ test/test_image.py
@@ -1,10 +1,13 @@
 import unittest
-from unittest.mock import MagicMock
-from PIL import Image as PILImage
-from WebUI.Image import Custom_Image
 from datetime import datetime
+from unittest.mock import MagicMock
+
+from WebUI.Image import Custom_Image
+
 
 class TestCustomImage(unittest.TestCase):
+    """Class to test the Custom_Image module."""
+
     def setUp(self):
         self.test_image_path = "./test/test.jpg"
         self.custom_image = Custom_Image(self.test_image_path)
@@ -16,7 +19,7 @@ class TestCustomImage(unittest.TestCase):
 
     def test_load_image_from_nextcloud(self):
         mock_instance = MagicMock()
-        
+
         self.custom_image.nextcloud_instance = mock_instance
         with open("./test/test.jpg", "rb") as t_img:
             return_val = t_img.read()
@@ -33,18 +36,19 @@ class TestCustomImage(unittest.TestCase):
 
     def test_extract_date(self):
         mock_instance = MagicMock()
-        
+
         self.custom_image.nextcloud_instance = mock_instance
         with open("./test/test.jpg", "rb") as t_img:
             return_val = t_img.read()
         mock_instance.files.download.return_value = return_val
 
-        self.assertEqual(self.custom_image.get_date(), '2024-10-07 10:47:21')
+        self.assertEqual(self.custom_image.get_date(), "2024-10-07 10:47:21")
 
     def test_get_date(self):
-        self.custom_image.date = datetime.strptime('2023:01:01', '%Y:%m:%d')
-        self.assertEqual(self.custom_image.get_date(), '2023-01-01 00:00:00')
+        self.custom_image.date = datetime.strptime("2023:01:01", "%Y:%m:%d")
+        self.assertEqual(self.custom_image.get_date(), "2023-01-01 00:00:00")
         # Implement this test to check if the date is correctly formatted and returned as a string
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
